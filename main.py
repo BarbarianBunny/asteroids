fbccb354eeaa592049ff0e65a9ec47a093078297
BarbarianBunny <carury@gmail.com>
2025-01-05 21:19:38 -0800

    add player to game screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -11,15 +12,17 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill((0, 0, 0))
     clock = pygame.time.Clock()
     dt = 0  # DeltaTime in seconds
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        player.draw(screen)
+
         pygame.display.flip()  # Updates display, call last
         dt = clock.tick(60) / 1000  # Wait 1/60th of a sec
 
