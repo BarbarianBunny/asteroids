c0cb9e2cc956db19ffabe213578a0f850e16ffaa
BarbarianBunny <carury@gmail.com>
2025-01-05 21:07:46 -0800

    update main.py tick every 60 secs

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0  # DeltaTime in seconds
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,6 +21,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()  # Updates display, call last
+        dt = clock.tick(60) / 1000  # Wait 1/60th of a sec
 
 
 if __name__ == "__main__":
